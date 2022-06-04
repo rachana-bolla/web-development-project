49cfef0125a4895a77892cb100363baaab58e05c
rachana-bolla <42509417+rachana-bolla@users.noreply.github.com>
2022-06-04 12:38:22 -0700

    Update pages,image add,validation for contact

---
+++ validation.js
@@ -0,0 +1,48 @@
+function validateFrom()
+{
+   var tuname=document.getElementById("username").value;
+   var temail=document.getElementById("emailAddress").value;
+   var tmsg=document.getElementById("message").value;
+
+   var text;
+   var status=true;
+   if(!validateNull(tuname))
+   {
+           text=text+" Please Enter UserName";
+           status=false;
+   }
+   if(!validateNull(temail))
+   {
+    text=text+" Please Enter EmailAddress";
+    status=false;
+   }
+   if(!validateNull(tmsg))
+   {
+    text=text+" Please Enter Message";
+    status=false;
+   }
+
+   if(status)
+   {
+   console.log("user Name...."+tuname);
+   console.log("Email Address...."+temail);
+   console.log("Message..."+tmsg);
+   }
+}
+
+function validateNull(formelement)
+{
+    if(formelement==="")
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+function validateEmail(email)
+{
+
+}
